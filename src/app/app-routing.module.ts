d5a2246874e8127e030705840e958794a24ee3a7
shivanidesai14 <shivanidesai402@gmail.com>
2019-03-20 23:42:54 +0530

    payment pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,9 @@ const routes: Routes = [
   { path: 'payment-options', loadChildren: './payment-options/payment-options.module#PaymentOptionsPageModule' },
   { path: 'my-profile', loadChildren: './my-profile/my-profile.module#MyProfilePageModule' },
   { path: 'select-toll-plaza', loadChildren: './pages/select-toll-plaza/select-toll-plaza.module#SelectTollPlazaPageModule' },  { path: 'payment-method', loadChildren: './pages/payment-method/payment-method.module#PaymentMethodPageModule' },
+  { path: 'payment-details', loadChildren: './pages/payment-details/payment-details.module#PaymentDetailsPageModule' },
+  { path: 'payment-details-add', loadChildren: './pages/payment-details-add/payment-details-add.module#PaymentDetailsAddPageModule' },
+  { path: 'transection-status', loadChildren: './pages/transection-status/transection-status.module#TransectionStatusPageModule' },
 
 ];
 
