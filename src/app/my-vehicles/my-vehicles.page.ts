20c586e1a22b5b1a2947ea162ca478a91a0390c7
Zeel Modi <zeel91297@gmail.com>
2019-04-13 16:25:12 +0530

    changes added to forget password, added defaultHref

--- src/app/my-vehicles/my-vehicles.page.ts
+++ src/app/my-vehicles/my-vehicles.page.ts
@@ -1,22 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   MenuController,
   ModalController,
   AlertController,
   ToastController
-} from '@ionic/angular';
+} from "@ionic/angular";
 
-import { AddVehicleModelPage } from './add-vehicle-model/add-vehicle-model.page';
-import { VehicledbProvider } from '../providers/vehicledb/vehicledb';
-import { vehicle } from '../providers/classes/classVehicle';
-import { vehicleTypeProvider } from '../providers/vehicledb/vehicleType';
-import { vehicleType } from '../providers/classes/classVehicleType';
-import { FormControl } from '@angular/forms';
+import { AddVehicleModelPage } from "./add-vehicle-model/add-vehicle-model.page";
+import { VehicledbProvider } from "../providers/vehicledb/vehicledb";
+import { vehicle } from "../providers/classes/classVehicle";
+import { vehicleTypeProvider } from "../providers/vehicledb/vehicleType";
+import { vehicleType } from "../providers/classes/classVehicleType";
+import { FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-my-vehicles',
-  templateUrl: './my-vehicles.page.html',
-  styleUrls: ['./my-vehicles.page.scss']
+  selector: "app-my-vehicles",
+  templateUrl: "./my-vehicles.page.html",
+  styleUrls: ["./my-vehicles.page.scss"]
 })
 export class MyVehiclesPage implements OnInit {
   vehicles: vehicle[] = [];
@@ -24,6 +24,8 @@ export class MyVehiclesPage implements OnInit {
   vehicle_type: vehicleType[] = [];
   searchVehicle: FormControl;
   uid: any;
+
+  showList: boolean = false;
   constructor(
     private menuController: MenuController,
     private alertController: AlertController,
@@ -36,17 +38,20 @@ export class MyVehiclesPage implements OnInit {
   }
 
   async ngOnInit() {
-    this.uid = localStorage.getItem('id');
+    this.uid = localStorage.getItem("id");
     this.menuController.enable(true);
 
     const toast = await this.toastController.create({
-      message: 'Your Vehicles Retrieved',
+      message: "Your Vehicles Retrieved",
       duration: 2000
     });
     this._vehicle.getVehicleByUser(this.uid).subscribe(
       (data: vehicle[]) => {
         this.vehicles = data;
         this.vehiclesDup = data;
+        if (vehicle.length == 0) {
+          this.showList = true;
+        }
       },
       err => {
         console.log(err);
@@ -88,7 +93,7 @@ export class MyVehiclesPage implements OnInit {
   getItems(ev) {
     this.vehicles = this.vehiclesDup;
     const val = ev.target.value;
-    if (val && val.trim() !== '') {
+    if (val && val.trim() !== "") {
       this.vehicles = this.vehicles.filter(
         x =>
           x.vehicle_no.toLocaleLowerCase().indexOf(val.toLocaleLowerCase()) > -1
@@ -98,16 +103,16 @@ export class MyVehiclesPage implements OnInit {
 
   async deleteVehicle(vehicle_no: number) {
     const confirmBox = await this.alertController.create({
-      header: 'Confirm!',
-      message: 'Are you sure you want to delete this vehicle?',
+      header: "Confirm!",
+      message: "Are you sure you want to delete this vehicle?",
       buttons: [
         {
-          text: 'No',
-          role: 'cancel',
-          cssClass: 'secondary'
+          text: "No",
+          role: "cancel",
+          cssClass: "secondary"
         },
         {
-          text: 'Yes',
+          text: "Yes",
           handler: () => {
             this._vehicle.deleteVehicleByUser(vehicle_no).subscribe(
               (data: any) => {
@@ -117,7 +122,7 @@ export class MyVehiclesPage implements OnInit {
                 console.log(err);
               },
               () => {
-                console.log('deleted');
+                console.log("deleted");
               }
             );
           }
