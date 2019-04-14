bcc8a34c39b0054697fe56da99c5913aaca15347
Zeel Modi <zeel91297@gmail.com>
2019-04-14 22:43:11 +0530

    signup button color changes, menu control disabled on some pages

--- src/app/homeselect/homeselect.page.ts
+++ src/app/homeselect/homeselect.page.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Router, NavigationExtras, ActivatedRoute } from "@angular/router";
 import { TollplazaService } from "../../app/providers/tollplazadb/tollplaza.service";
 import { Tollplazza } from "../../app/shared/tollplaza_class";
-import { ToastController } from '@ionic/angular';
-
+import { ToastController, MenuController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-homeselect',
-  templateUrl: './homeselect.page.html',
-  styleUrls: ['./homeselect.page.scss'],
+  selector: "app-homeselect",
+  templateUrl: "./homeselect.page.html",
+  styleUrls: ["./homeselect.page.scss"]
 })
 export class HomeselectPage implements OnInit {
   tid: any;
@@ -20,15 +19,21 @@ export class HomeselectPage implements OnInit {
   final_tollplaza: Tollplazza[] = [];
   isComeFromSourceAndDestinationSegment: boolean = false;
   toll_ids: number[] = [];
-  constructor(public router: Router, private activateRoute: ActivatedRoute, public tpdata: TollplazaService, public toast: ToastController) {
+  constructor(
+    public router: Router,
+    private activateRoute: ActivatedRoute,
+    public tpdata: TollplazaService,
+    public toast: ToastController,
+    public menuCtrl: MenuController
+  ) {
+    this.menuCtrl.enable(false);
     if (this.router.getCurrentNavigation().extras.state) {
       this.isComeFromSourceAndDestinationSegment = true;
       this.toll_ids = this.router.getCurrentNavigation().extras.state.toll_ids;
+    } else {
+      this.tid = this.activateRoute.snapshot.paramMap.get("myid");
     }
-    else {
-      this.tid = this.activateRoute.snapshot.paramMap.get('myid');
-    }
-    this.plaza=[];
+    this.plaza = [];
   }
   onClick() {
     this.final_tollplaza = [];
@@ -48,44 +53,47 @@ export class HomeselectPage implements OnInit {
         finalplaza: this.final_tollplaza
       }
     };
-    this.router.navigate(["/varify-rout", {
-      prev_tid: this.tid,
-    }], navigationExtras);
+    this.router.navigate(
+      [
+        "/varify-rout",
+        {
+          prev_tid: this.tid
+        }
+      ],
+      navigationExtras
+    );
   }
   onSelect(num) {
     var index = this.plazaid.indexOf(num);
-    if (index > -1)
-      this.plazaid.splice(index, 1);
-    else
-      this.plazaid.push(num);
+    if (index > -1) this.plazaid.splice(index, 1);
+    else this.plazaid.push(num);
     this.buttonDisabled = true;
   }
 
   ngOnInit() {
-    this.plaza=[];
+    this.plaza = [];
     if (!this.isComeFromSourceAndDestinationSegment) {
       this.tpdata.getAllTollPlazaById(this.tid).subscribe((data: any[]) => {
         this.plaza.push(data[0]);
       });
-      this.tpdata.getConnectedPlaza(this.tid).subscribe((data: any[]) => {
-        // this.plaza=data;
-        data.forEach(element => {
-          this.plaza.push(element);
-        });
-      },
-        function (err) {
+      this.tpdata.getConnectedPlaza(this.tid).subscribe(
+        (data: any[]) => {
+          // this.plaza=data;
+          data.forEach(element => {
+            this.plaza.push(element);
+          });
+        },
+        function(err) {
           console.log(err);
         },
-        function () {
-        });
-    }
-    else {
+        function() {}
+      );
+    } else {
       this.toll_ids.forEach(element => {
-          this.tpdata.getAllTollPlazaById(element).subscribe((data:any[])=>{
-            this.plaza.push(data[0]);
-          });
+        this.tpdata.getAllTollPlazaById(element).subscribe((data: any[]) => {
+          this.plaza.push(data[0]);
+        });
       });
     }
   }
-
 }
